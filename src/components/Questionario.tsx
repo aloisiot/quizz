c0e56cb6dc4238714ebdd6497134e4ece9a2dece
Aloísio Carvalho <loistc.carvalho@gmail.com>
2022-01-15 17:05:12 -0300

    diminuindo altura de opçao de resposta e almentando o tempo para responder

--- src/components/Questionario.tsx
+++ src/components/Questionario.tsx
@@ -23,7 +23,7 @@ export default function Questionario(props: QuestionarioProps){
             {props.questao ? (
                 <Questao
                     valor={props.questao}
-                    tempoParaResponder={6}
+                    tempoParaResponder={8}
                     tempoEsgotado={props.irProximoPasso}
                     respostaFornecida={respostaFornecida} />
                 ) : false
